5f5995ff963b9e371c357716960e74c12ab5cbeb
Kimball Germane <krgermane@yahoo.com>
2012-12-11 15:14:43 -0700

    good scoring function

--- scoring.rkt
+++ scoring.rkt
@@ -9,11 +9,16 @@
     [2 5]
     [3 15]
     [4 60]
-    [5 120]
+    [5 240]
     [else (worth 2)]))
 
 (define (points week-received week-due stars)
   (* (worth stars) (/ (- 14 (max (- week-received week-due) 0)) 14)))
 
-(define (grade-for-total-points points)
-  points)
+(define (grade-for-total-points p)
+  (cond
+    [(< p 1500) (exact->inexact (/ p 2000))]
+    [(< p 2000) (exact->inexact (+ (/ p 5000) 9/20))]
+    [else (let ([a (* 3/20 (exp 8/3))]
+                [b -1/750])
+            (* a (exp (* b p))))]))

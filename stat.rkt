9cebe8a729b0d0547c803a9f9c36e7b303bf8ee4
Jay McCarthy <jay.mccarthy@gmail.com>
2013-04-01 10:06:59 -0600

    stat

--- stat.rkt
+++ stat.rkt
@@ -2,8 +2,10 @@
 (require racket/contract
          racket/file
          racket/list
+         racket/format
          racket/match
          racket/runtime-path
+         racket/function
          "score.rkt")
 
 (define-runtime-path curriculum-file "curriculum.sexp")
@@ -11,19 +13,29 @@
 (define stars
   (map
    (λ (chapter-exercises)
-     (map
-      (match-lambda
-        [(list _ star _ _ _) star])
-      (second chapter-exercises)))
+     (cons (first chapter-exercises)
+           (map
+            (match-lambda
+             [(list _ star _ _ _) star])
+            (second chapter-exercises))))
    (file->value curriculum-file)))
 
-
-(map
- (λ (n)
-   (* (point-worth n)
-      (apply +
-             (map
-              (λ (exs)
-                (count (=/c n) exs))
-              stars))))
- (list 1 2 3 4 5))
+(module+ main
+  (for/fold ([total 0])
+      ([e (in-list stars)])
+    (match-define (cons ch exs) e)
+    (printf "~a: " (~a #:align 'right #:min-width 12 ch))
+    (define ch-total
+      (for/sum ([n (in-list '(1 2 3 4 5))])
+        (define n-exs
+          (count (curry = n) exs))
+        (printf "~a (~a) "
+                n (~a #:align 'right #:min-width 2 n-exs))
+        (* (point-worth n) n-exs)))
+    (define next-total
+      (+ total ch-total))
+    (printf "\t~a\t~a" 
+            (~a #:align 'right #:min-width 3 ch-total)
+            (~a #:align 'right #:min-width 4 next-total))
+    (newline)
+    next-total))
